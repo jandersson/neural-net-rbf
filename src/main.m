f7f2681b99a389ed0d5c99d02d187c0e5c3acf6d
Jokull Johannsson <jokull@kth.se>
2016-02-04 10:14:40 +0100

    Some small changes

--- src/main.m
+++ src/main.m
@@ -2,23 +2,27 @@ clear all
 close all
 
 x = 0:0.1:2*pi;
-x = linspace(0,2*pi,6)
 x = x';
-units = 6; % Number of neurons (in hidden layer)
-f = sin2x(x);
-%f = square(2*x)
-%%
+units = 60; % Number of neurons (in hidden layer)
+%f = sin2x(x);
+f = square(2*x)
 makerbf
 
-%%
 Phi = calcPhi(x,m,var)
 
 w = (Phi'*Phi)\(Phi'*f)
 
 y = Phi*w
 
-rbfplot1(x,y,f,units)
+%y = sign(y)
 
+
+
+rbfplot1(x,y,f,units)
+%%
+fun = 'sin2x';
+eta = 0.25;
+diter
 
 
 
